4f82f45c2f5662410b8b9798c5e365e5a9b44fb6
xorob0 <timothee.simon@openmailbox.org>
2017-01-07 23:36:11 +0100

    Creatted partitionning.sh

---
+++ partitionning.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+function ask_disk()
+{
+	echo "Wich of these disks do you want to partition ?"
+	ls /dev/sd[a-z]
+	read DISK_NUMBER
+}
+
+function partition_disk()
+{
+	# Launch interactive partitionning tool
+	cfdisk $DISK_NUMBER &> /dev/null
+}
+
+function verify_partitionning()
+{
+	# List available partitions
+	fdisk -l
+	echo "Are you sure of your partitonning ? (Y/n)"
+	read ANS_SURE
+	case $ANS_SURE in
+		# If sure, go on to the formating
+		Y) bash formating.sh
+			;;
+		y) bash formating.sh
+			;;
+		# If unsure, start over
+		N) ask_disk
+			;;
+		Y) ask_disk
+			;;
+	esac
+}
+
+ask_disk
+clear
+
+partition_disk
+clear
+
+verify_partitionning
+clear
