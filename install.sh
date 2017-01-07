f8d276c32ee0e543f2f2b72d6e1e9c067c0fd12d
xorob0 <timothee.simon@openmailbox.org>
2017-01-07 22:35:56 +0100

    added some function and clean up

--- install.sh
+++ install.sh
@@ -1,9 +1,57 @@
 #!/bin/bash
-loadkeys fr &> /dev/null
-if ping -c 1 195.238.2.21 &> /dev/null
-then
-	echo "No need for a configuration, you are already connected to the internet !"
-else
-	echo "We need to configure your internet connection."
-fi
 
+function keyboard()
+{
+	# Load azerty keyboard
+	loadkeys fr &> /dev/null
+}
+
+function internet()
+{
+	# Check internet connection and lauch connection script if not connected
+	if ping -c 1 195.238.2.21 &> /dev/null
+	then
+		echo "No need for a configuration, you are already connected to the internet !"
+		read a
+	else
+		echo "We need to configure your internet connection."
+		read a
+	fi
+}
+
+function boot_verif()
+{
+	# Verify boot mode
+	if [ ! -d "/sys/firmware/efi/efivars/" ]
+	then
+		# if not in EFI mode return error 1
+		echo "Please boot in EFI mode"
+		read a
+		exit
+	fi
+}
+
+function time_sync()
+{
+	# Update the system clock
+	timedatectl set-ntp true &> /dev/null
+}
+
+function partitionning()
+{
+	# Ask if partitionning is needed
+	echo "Do you want to partiton your disk(s) ? (Y/n)"
+	CHOIX_PARTITION="Y"
+	read CHOIX_PARTITION
+	echo "$CHOIX_PARTITION"
+	case "$CHOIX_PARTITION" in
+		Y) echo "Partitionning"
+			;;
+		y) echo "Partitionning"
+			;;
+		N) echo "Not partitionning"
+			;;
+		n) echo "Not partitionning"
+			;;
+	esac
+}
