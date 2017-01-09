95fe642d74aed3846a6d7a18ddb920425d7c4482
xorob0 <timothee.simon@openmailbox.org>
2017-01-09 01:08:31 +0100

    corrected error

--- packages.sh
+++ packages.sh
@@ -51,13 +51,13 @@ function install_connman()
 
 	# Create config file
 	arch-chroot /mnt mkdir /etc/connman
-	arch-chroot /mnt echo "[General]" >> /etc/connman/main.conf
+	echo "[General]" >> /etc/connman/main.conf
 
 	# Add config to prefer ethernet over wifi
-	arch-chroot /mnt echo "PreferrefTechnologies=ethernet,wifi" >> /etc/connman/main.conf
+	echo "PreferrefTechnologies=ethernet,wifi" >> /etc/connman/main.conf
 
 	# Add config to blacklist vitual interfaces
-	arch-chroot /mnt echo "NetworkInterfaceBlacklist=vmnet,vboxnet,virbr,ifb,docker,veth,eth,wlan" >> /etc/connman/main.conf
+	echo "NetworkInterfaceBlacklist=vmnet,vboxnet,virbr,ifb,docker,veth,eth,wlan" >> /etc/connman/main.conf
 }
 
 function install_ucode()
